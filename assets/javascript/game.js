bb9ceca28f595dc4237f725761de11544923a173
antfats <antfats@hotmail.com>
2019-02-26 12:22:14 -0700

    Questions and answers

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var questions = [{
+
+        question: "What does the <h1> tag do in html",
+        choices: ["Capitalizes everything", "Formats the text", "Makes everything inside a header", "Nothing"],
+        validAnswer = 2
+    }, {
+        question: "How do I call a function in javascript",
+        choices: ["functionname();", "functionname.call();", "call $('functionname')", "functionname;"],
+        validAnswer = 0
+    }, {
+        question: "How do we call an element with jquery?",
+        choices: ["$(#elementName)", "Formats the text", "Makes everything inside a header", "Nothing"],
+        validAnswer = 2
+    }, {
+        question: "What method converts a string into a value?",
+        choices: [".stringToInt", ".int", ".append", ".parseInt"],
+        validAnswer = 3
+    }
+    ]
+});
