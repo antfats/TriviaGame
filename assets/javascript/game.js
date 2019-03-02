29fbf7e75ea61474946bba5f8c8b42e5f93ac296
bob <antfats@hotmail.com>
2019-03-01 17:13:13 -0700

    Lots of progress!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,55 @@
 $(document).ready(function () {
     //Questions here!
     var questions = [{
-        question: "What does the <h1> tag do in html",
-        choices: ["Capitalizes everything", "Formats the text", "Makes everything inside a header", "Nothing"],
-        validAnswer: 2
+        question: 'What does the < h1 > tag do in html',
+
+        answers:
+        {
+            choiceA: "Capitalizes everything ",
+            choiceB: "Formats the text ",
+            choiceC: "Makes everything inside a header ",
+            choiceD: "Nothing ",
+
+        }
+
     }, {
         question: "How do I call a function in javascript",
-        choices: ["functionname();", "functionname.call();", "call $('functionname')", "functionname;"],
-        validAnswer: 0
+
+        answers: {
+            choiceA: "functionname(); ",
+            choiceB: "functionname.call(); ",
+            choiceC: "call $('functionname') ",
+            choiceD: "functionname; ",
+        }
     }, {
         question: "How do we call an element with jquery?",
-        choices: ["$(#elementName)", "Formats the text", "Makes everything inside a header", "Nothing"],
-        validAnswer: 2
+
+        answers: {
+            choiceA: "$(#elementName) ",
+            choiceB: "Formats the text ",
+            choiceC: "Makes everything inside a header ",
+            choiceD: "Nothing ",
+        }
+
+
     }, {
         question: "What method converts a string into a value?",
-        choices: [".stringToInt", ".int", ".append", ".parseInt"],
-        validAnswer: 3
+        answers: {
+            choiceA: ".stringToInt ",
+            choiceB: ".int ",
+            choiceC: ".append ",
+            choiceD: ".parseInt ",
+        }
     }
     ]
 
     //timer
+    var running = false;
     var intervalId;
     var counter = 0;
     var timeLeft = 30;
     var timer = $("#time");
-    // timer.html(timeLeft - counter);
-
-    // timeIt(function () {
-
+    // timer
     function timeIt() {
         counter++;
         timer.html(timeLeft - counter);
@@ -38,26 +60,65 @@ $(document).ready(function () {
     }
 
 
-
-
-
-
-    //   if (count <= 0)
-    //   {
-    //      clearInterval(counter);
-    //      return;
-    //   }
-
-
-
-
-
-    //.click function to start the game
+    //Starts the clock, as well as starts the for loop that puts the game on the screen
     $("#button").click(function () {
         intervalFunction();
-        $(".content").append(questions[1]);
+        if (running = false) {
+            running = true;
+        }
+        else if (timeLeft === 0) {
+            running = false;
+            clearInterval;
+            alert("Times up!");
+        }
 
-    })
+        // for loop to append all questions 
 
 
-});
+        for (var currentQuestion = 0; currentQuestion < questions.length; currentQuestion++) {
+            function display() {
+
+                var choicesOne = questions[currentQuestion].answers;
+
+
+                answerOne = $("<p class='choices'>");
+                questionDiv = $("<p class='questions'>" + (answerOne));
+
+                questionDiv.append(questions[currentQuestion].question)
+
+                answerOne.append(questions[currentQuestion].answers);
+                console.log(choicesOne);
+                $("#questionDiv").append(questionDiv);
+                $("#answerDiv").append(answerOne);
+
+
+                function checkAnswer(answer) {
+                    if (this.click === questions[currentQuestion].answers[2]) {
+                        alert("Woo! That was right");
+                        wins++;
+                    }
+                    else if (this.click === questions[currentQuestion].answers[0]) {
+                        alert("Nice!");
+                        wins++
+                    }
+                    else if (this.click === questions[currentQuestion].answers[0]) {
+                        alert("Nice!");
+                        wins++
+
+                    }
+                    else if (this.click === questions[currentQuestion].answers[3]) {
+                        alert("Nice!");
+                        wins++;
+                    }
+                    $(".choices").click(function () {
+
+
+                    });
+                }
+                var questionOne = questions[currentQuestion].question;
+            }
+            display();
+
+        }
+    });
+})
